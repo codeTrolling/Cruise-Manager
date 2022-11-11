d34e3d24c198c9b74f6a47996e7151e27f1542ad
Your name <wedoulilishte@gmail.com>
2022-11-11 23:35:55 +0200

    Update ticket type form

---
+++ HTML_CSS_JS/ReservationScript.js
@@ -0,0 +1,18 @@
+let type = document.getElementById("ticket-type");
+type.addEventListener("click", () => {
+    if(document.getElementById("options").style.display == "block")
+    {
+        document.getElementById("options").style.display = "none";
+    }
+    else{
+        document.getElementById("options").style.display = "block";
+    }
+    if(document.getElementById("options").style.display == "block")
+    {
+        type.style.borderBottomWidth = "1px";
+        type.style.borderRadius = "20px 20px 0px 0px";
+    }
+    else{
+        type.removeAttribute("style");
+    }
+})
