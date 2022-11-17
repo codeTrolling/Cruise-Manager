7e43abb25d4751025d292d7cff6c320cfee73837
Your name <wedoulilishte@gmail.com>
2022-11-17 23:02:23 +0200

    Add forms to users page

---
+++ HTML_CSS_JS/UsersScript.js
@@ -0,0 +1,9 @@
+let formState = "sign in";
+const signInBtn = document.getElementById("signin");
+const signUpBtn = document.getElementById("signup");
+ 
+signInBtn.addEventListener("click", () =>{
+ 
+})
+ 
+//TODO: make it so sign up swipes to the left while sign in comes from the right and vice versa
