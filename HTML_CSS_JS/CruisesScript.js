654741ce2f8bfcd5649b14a1be613118ec6192a5
Your name <wedoulilishte@gmail.com>
2022-11-20 21:51:55 +0200

    I have lost this battle

--- HTML_CSS_JS/CruisesScript.js
+++ HTML_CSS_JS/CruisesScript.js
@@ -8,5 +8,22 @@ function showOrHide(){
     }
 }
 
+// function changeBoxSize(){
+//     if(document.getElementsByClassName("details-container")[0].getAttribute(innerHeight) != document.getElementsByClassName("general-cruise-info")[0].getAttribute(innerHeight) && document.getElementById("details1").style.display == "none"){
+//         document.getElementsByClassName("details-container")[0].style.height = document.getElementsByClassName("general-cruise-info")[0].getAttribute(innerHeight);
+//         console.log("works");
+//     }
+//     console.log("in function");
+// }
+
 const detailsbtn = document.getElementById("details")
 detailsbtn.addEventListener("click", showOrHide);
+
+// const observer = new MutationObserver(entries => {
+//     changeBoxSize();
+// })
+
+// const testObj = document.getElementById("testid");
+// observer.observe(testObj, { attributes: true});
+
+// testObj.addEventListener("resize", changeBoxSize)
