be577a2ac008b5085f0856b1d657bbb295d57463
Your name <wedoulilishte@gmail.com>
2022-11-22 19:33:27 +0200

    Improve design/animation

--- HTML_CSS_JS/UsersScript.js
+++ HTML_CSS_JS/UsersScript.js
@@ -5,6 +5,7 @@ let signIn = document.getElementById("form");
 let signUp = document.getElementById("sign-up-form");
 const resetPass = document.getElementById("reset-pass");
 const borders = document.getElementById("input-borders");
+const background = document.getElementById("background-img");
  
 function switchBtwnLogins(moveTime, moveDistance, btnToRmv, btnToAdd, color, resDelay, resOpacity, borderTime, borderWidth){
     signIn.style.transition = moveTime;
@@ -31,8 +32,43 @@ function switchBtwnLogins(moveTime, moveDistance, btnToRmv, btnToAdd, color, res
     borders.style.height = borderWidth;
 }
 
+function focusForm(backgroundTime, backgroundBlur, formBackgroundTime, formBackgroundColor){
+    background.style.transition = backgroundTime;
+    background.style.filter = backgroundBlur;
+    borders.style.transition = formBackgroundTime;
+    borders.style.backgroundColor = formBackgroundColor;
+}
+
 signInBtn.addEventListener("click", (event) => {switchBtwnLogins("0.5s ease-in-out", "translateX(0px)", signUpBtn, signInBtn, "rgb(0, 170, 255)", "0.3s", "1", "0.4s linear", "180px")})
 signUpBtn.addEventListener("click", (event) => {switchBtwnLogins("0.5s ease-in-out", "translateX(-484px)", signInBtn, signUpBtn, "rgb(0, 170, 255)", "0s", "0", "0.4s linear", "490px")})
+
+let inputs = document.querySelectorAll("input");
+
+inputs.forEach(element => {
+    element.addEventListener("focusin", (event) => focusForm("0.3s linear", "blur(5px)", "background-color 0.5s linear", "rgba(255, 248, 240, 1)"));
+})
+
+inputs.forEach(element => {
+    element.addEventListener("focusout", (event) => focusForm("0.3s linear", "blur(0px)", "background-color 0.5s linear", "rgba(255, 248, 240, 0"));
+})
+
+// inputs.forEach(element => {
+//     element.addEventListener("focusin", () => {
+//         background.style.transition = "0.1s linear"
+//         background.style.filter = "blur(5px)"
+//         document.getElementById("input-borders").style.transition = "0.1s linear"
+//         document.getElementById("input-borders").style.backgroundColor = "rgb(255, 248, 240)"
+//     })
+// });
+ 
+// inputs.forEach(element => {
+//     element.addEventListener("focusout", () => {
+//         background.style.transition = "0.1s linear"
+//         background.style.filter = "blur(0px)"
+//         document.getElementById("input-borders").style.transition = "0.2s linear"
+//         document.getElementById("input-borders").style.backgroundColor = "transparent"
+//     })
+// });
 
 // signInBtn.addEventListener("click", () =>{
 //  if(formState !== "sign in"){
