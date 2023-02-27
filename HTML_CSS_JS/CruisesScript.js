c97453b80a6503e5c3828796dea5ea0873aa7d16
Your name <wedoulilishte@gmail.com>
2023-02-27 22:23:08 +0200

    Add backend and modify cruises tab

--- HTML_CSS_JS/CruisesScript.js
+++ HTML_CSS_JS/CruisesScript.js
@@ -1,5 +1,7 @@
+var cruiseWindows = document.querySelectorAll(".details-container");
+
 function showOrHide(){
-    let details = document.getElementById("details1");
+    var details = document.getElementById("details1");
     if(details.style.display == "block"){
         details.style.display = "none";
     }
@@ -16,7 +18,7 @@ function showOrHide(){
 //     console.log("in function");
 // }
 
-const detailsbtn = document.getElementById("details")
+const detailsbtn = document.getElementById("show-details")
 detailsbtn.addEventListener("click", showOrHide);
 
 // const observer = new MutationObserver(entries => {
@@ -27,3 +29,29 @@ detailsbtn.addEventListener("click", showOrHide);
 // observer.observe(testObj, { attributes: true});
 
 // testObj.addEventListener("resize", changeBoxSize)
+
+// console.log(cruiseWindows[0].offsetHeight);
+// cruiseWindows[0].style.height = "500px";
+// console.log(cruiseWindows[0].offsetHeight);
+// cruiseWindows[0].style.height = cruiseWindows[0].offsetHeight;
+ 
+// cruiseWindows[0].addEventListener("resize", () => {
+//     console.log("resizes");
+// })
+ 
+// window.onresize = () => {
+//     console.log(window.getComputedStyle(cruiseWindows[0]).getPropertyValue("height"), "before none")
+//     cruiseWindows[0].style.height = "";
+//     console.log(window.getComputedStyle(cruiseWindows[0]).getPropertyValue("height"), "on none")
+//     cruiseWindows[0].style.height = window.getComputedStyle(cruiseWindows[0]).getPropertyValue("height");
+//     console.log(window.getComputedStyle(cruiseWindows[0]).getPropertyValue("height"), "after first")
+//     cruiseWindows[0].style.transition = "all 0.5 linear";
+//     console.log(window.getComputedStyle(cruiseWindows[0]).getPropertyValue("height"))
+//     console.log(details);
+//     if(details.style.display == "block"){
+//         //cruiseWindows[0].style.height += details.offsetHeight;
+//         //cruiseWindows[0].style.height = "400px";
+//         cruiseWindows[0].style.height = window.getComputedStyle(cruiseWindows[0].getPropertyValue("height"));
+//         console.log(window.getComputedStyle(cruiseWindows[0]).getPropertyValue("height"), "in if statement");
+//     }
+// }
